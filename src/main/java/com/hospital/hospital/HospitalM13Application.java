2f8bfb6d11a39e06a9965167a46d977283a50df3
Pau Carrera <poksy2002@gmail.com>
2024-10-03 15:29:11 +0200

    first commit

---
+++ src/main/java/com/hospital/hospital/HospitalM13Application.java
@@ -0,0 +1,13 @@
+package com.hospital.hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalM13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalM13Application.class, args);
+	}
+
+}
