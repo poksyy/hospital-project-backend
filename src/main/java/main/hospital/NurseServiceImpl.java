a579c2a6f7eea5df4e6e7f8b5267b9a013325a99
Dylan Navarro Vinyarta <159908255+Dynavy@users.noreply.github.com>
2024-10-03 16:30:46 +0200

    Feature: added the nurses information.

--- src/main/java/main/hospital/NurseServiceImpl.java
+++ src/main/java/main/hospital/NurseServiceImpl.java
@@ -1,5 +1,21 @@
 package main.hospital;
 
-public class NurseServiceImpl {
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Service;
 
+@Service
+public class NurseServiceImpl  {
+
+	private List<Nurse> nurses = new ArrayList<>();
+
+	// Constructor that initializes instances of Nurse to the ArrayList nurses.
+	public NurseServiceImpl() {
+		// // We need to pass the same parameters that are expected by the Nurse
+		// constructor.
+		nurses.add(new Nurse("Pau", "pl2024769", "pau1234"));
+		nurses.add(new Nurse("Dylan", "pl2024379", "dylan1234"));
+		nurses.add(new Nurse("Cristian", "pl2024768", "cristian1234"));
+
+	}
 }
