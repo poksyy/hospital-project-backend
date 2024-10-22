6832a5fda63a0bb63244eb0db83a9d2a293d7efa
Dylan Navarro Vinyarta <159908255+Dynavy@users.noreply.github.com>
2024-10-22 17:01:00 +0200

    DOCS(NurseServiceImpl): commented the loginAuthentication() method.

--- src/main/java/main/hospital/NurseServiceImpl.java
+++ src/main/java/main/hospital/NurseServiceImpl.java
@@ -12,58 +12,106 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-
 @Service
 public class NurseServiceImpl implements NurseService {
 
-	 private SessionFactory sessionFactory;
-   
-   @Autowired
-   private NurseRepository nurseRepository; 
+	// Instance that will be used to create sessions and interact with the database. 	
+	private SessionFactory sessionFactory;
 
-    public NurseServiceImpl(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
+	// Autowired instance for the CRUD.
+	@Autowired
+	private NurseRepository nurseRepository;
 
-    public boolean LoginAuthentication(String user, String password) { 
-        Session session = sessionFactory.openSession();
-        Transaction transaction = null;
-        Nurse nurse = null;
+	public NurseServiceImpl(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
 
-        try {
-            transaction = session.beginTransaction();
-            String hql = "FROM Nurse WHERE user = :user AND password = :password"; 
-            Query<Nurse> query = session.createQuery(hql, Nurse.class);
-            query.setParameter("user", user); 
-            query.setParameter("password", password);
+	public boolean LoginAuthentication(String user, String password) {
+		
+		// We are getting a session to interact with the database.
+		Session session = sessionFactory.openSession();
+		// We start a transaction instance to guarantee security of the database operations.
+		Transaction transaction = null;
+		// We create an instance of Nurse.
+		Nurse nurse = null;
 
-            nurse = query.uniqueResult();
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
+		try {
+			// Start transaction.
+			transaction = session.beginTransaction();
+			// HQL QUERY to search with user and name.
+			String hql = "FROM Nurse WHERE user = :user AND password = :password";
+			// We create the query waiting for a Nurse object response.
+			Query<Nurse> query = session.createQuery(hql, Nurse.class);
+			// Value of the query  for the user.
+			query.setParameter("user", user);
+			// Value of the query for the password.
+			query.setParameter("password", password);
 
-        if (nurse != null) {
-            System.out.println("Welcome to the application!.");
-            return true;
-        } else {
-            System.out.println("Invalid credentials.");
-            return false;
-        }
-    }
+			// Nurse object has only one(1) unique result.
+			nurse = query.uniqueResult();
+			// Transaction starts if everything is ok.
+			transaction.commit();
+		} catch (Exception TransactionError) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			TransactionError.printStackTrace();
+		// The session always ends regardless of whether the try or catch was executed.
+		} finally {
+			session.close();
+		}
+		// Query matched.
+		if (nurse != null) {
+			System.out.println("Welcome to the application!.");
+			return true;
+		// Query did not match.
+		} else {
+			System.out.println("Invalid credentials.");
+			return false;
+		}
+	}
 
 	public List<Nurse> getNursesInformation() {
 		return null;
 	}
 
 	public ResponseEntity<Nurse> findByName(String name) {
-		return ResponseEntity.notFound().build();
+		// Open a Hibernate session.
+	    Session session = sessionFactory.openSession();
+	    Transaction transaction = null;
+	    // Save the nurse selected to this variable.
+	    Nurse nurse = null;
+	    
+	    try {
+	    	// Start transaction.
+	        transaction = session.beginTransaction();
+	        // Create an HQL query to search nurse by name.
+	        String hql = "FROM Nurse WHERE name = :name";
+	        Query<Nurse> query = session.createQuery(hql, Nurse.class);
+	        query.setParameter("name", name);
+	        // Return only 1 result. If it returns more than 1 result it will launch an exception.
+	        nurse = query.uniqueResult();
+	        transaction.commit();
+	    } catch (Exception e) {
+	        if (transaction != null) {
+	        // If there's an error do a rollback.
+	            transaction.rollback();
+	        }
+	        e.printStackTrace();
+	    } finally {
+	        session.close();
+	    }
+
+	    // If nurse is found.
+	    if (nurse != null) {
+	        System.out.println(nurse);
+	        return ResponseEntity.ok(nurse);
+	    } else { 
+	    // If nurse is NOT found.
+	        return ResponseEntity.notFound().build();
+	    }
 	}
+
 
 	@Override
 	public <S extends Nurse> S save(S entity) {
@@ -89,11 +137,14 @@ public class NurseServiceImpl implements NurseService {
 		return false;
 	}
 
+	// Overrides findAll() from NurseService.
+	// Retrieves all nurse records by calling nurseRepository's findAll(), 
+	// which extends CrudRepository.
 	@Override
-    public Iterable<Nurse> findAll() {
-        return nurseRepository.findAll();
-    }
-	
+	public Iterable<Nurse> findAll() {
+		return nurseRepository.findAll();
+	}
+
 	@Override
 	public Iterable<Nurse> findAllById(Iterable<Integer> ids) {
 		// TODO Auto-generated method stub
