7d5f2ed85cee640074655bb05d1fbf7d5ed8e026
Dynavy <dynavy@hotmail.com>
2025-02-01 19:20:57 +0100

    FEAT(NurseValidator): implemented new validation for password changes.

--- src/main/java/main/hospital/NurseValidator.java
+++ src/main/java/main/hospital/NurseValidator.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class NurseValidator {
 
+	// Validate password when registering a Nurse.
 	public boolean isValidPassword(String password) {
+
 		if (password == null || password.length() < 8) {
 			return false;
 		}
@@ -21,9 +23,27 @@ public class NurseValidator {
 		}
 		return true;
 	}
+
+	// Validate password when updating password.
+	public boolean isValidPassword(Nurse nurse, NurseRepository nurseRepository) {
+		if (nurse.getPassword() == null || nurse.getPassword().length() < 8) {
+			return false;
+		}
+		if (!nurse.getPassword().matches(".*[A-Z].*")) {
+			return false;
+		}
+		if (!nurse.getPassword().matches(".*[0-9].*")) {
+			return false;
+		}
+		if (!nurse.getPassword().matches(".*[!@#$%^&*()-_+=<>?].*")) {
+			return false;
+		}
+		return true;
+	}
 	
-	// Used when updating Nurse Name and 
+	// Used when updating Nurse Name and
 	public boolean isValidProfileChanges(Nurse nurse, NurseRepository nurseRepository) {
+
 		if (nurse.getUser() == null || nurse.getName() == null || nurse.getUser().trim().isEmpty()
 				|| nurse.getName().trim().isEmpty()) {
 			return false;
@@ -37,13 +57,14 @@ public class NurseValidator {
 
 	// Default validation for Nurse update
 	public boolean isValidNurseRegister(Nurse nurse) {
-	    return nurse.getName() != null && !nurse.getName().trim().isEmpty() &&
-	           nurse.getUser() != null && !nurse.getUser().trim().isEmpty() &&
-	           nurse.getPassword() != null && !nurse.getPassword().trim().isEmpty();
+
+		return nurse.getName() != null && !nurse.getName().trim().isEmpty() && nurse.getUser() != null
+				&& !nurse.getUser().trim().isEmpty() && nurse.getPassword() != null
+				&& !nurse.getPassword().trim().isEmpty();
 	}
 
 	// Validates if the Username already exists on the database.
 	public boolean isUsernameTaken(String user, NurseRepository nurseRepository) {
-	    return nurseRepository.existsByUser(user);
+		return nurseRepository.existsByUser(user);
 	}
 }
